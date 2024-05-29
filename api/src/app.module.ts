2821509595ea255c179326c0dd358db2a9e2f9d9
Heiler Nova <heilernov@outlook.com>
2024-05-29 16:11:38 -0500

    Add(API): modulo de los contradores

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
 import { ModelsModule } from './models/models.module';
+import { ControllersModule } from './controllers/controllers.module';
 
 @Module({
-  imports: [CommonModule, ModelsModule],
+  imports: [CommonModule, ModelsModule, ControllersModule],
   controllers: [AppController],
   providers: [AppService],
 })
